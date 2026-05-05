b18bf28270b3ca6e0e5b0696758cac81590dd632
Sahil Adit <aditsahil17@gmail.com>
2026-05-05 14:31:43 +0530

    key settings changed

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ClerkProvider } from "@clerk/clerk-react";
 
-
-const PUBLISHABLE_KEY = process.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
